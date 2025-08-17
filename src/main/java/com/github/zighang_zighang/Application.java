bdb361064b65500ce7ad27327cbb6e6e9cd1ef0f
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-17 18:58:34 +0900

    [ZH-12] chore(util): EnableSpringDataWebSupport 설정

--- src/main/java/com/github/zighang_zighang/Application.java
+++ src/main/java/com/github/zighang_zighang/Application.java
@@ -2,8 +2,10 @@ package com.github.zighang_zighang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application {
 
     public static void main(String[] args) {
