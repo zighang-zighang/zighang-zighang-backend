6578a48fa177cad87fbaa14d61384c9e77fadfb1
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-17 23:24:15 +0900

    [0.1.0] (ZH-12) 기본 설정 파일 및 필수 Bean 등록 (#5)

---
+++ src/main/java/com/github/zighang_zighang/global/property/RedisProperty.java
@@ -0,0 +1,36 @@
+package com.github.zighang_zighang.global.property;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisProperty {
+
+    @NotBlank
+    String host;
+
+    @Min(1)
+    @Max(65535)
+    @NotNull
+    Integer port;
+
+    @NotNull
+    String username;
+
+    @Nullable
+    String password;
+
+    @Min(0)
+    @NotNull
+    Integer database;
+}
