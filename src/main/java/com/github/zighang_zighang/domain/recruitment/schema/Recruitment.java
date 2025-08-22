95a3610ea8a37794869b44ac9799d1c62222e079
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-22 14:19:37 +0900

    [ZH-31] feat(recruitment): 공고 스키마 구현

---
+++ src/main/java/com/github/zighang_zighang/domain/recruitment/schema/Recruitment.java
@@ -0,0 +1,68 @@
+package com.github.zighang_zighang.domain.recruitment.schema;
+
+import com.github.zighang_zighang.domain.recruitment.constant.*;
+import com.github.zighang_zighang.global.infra.database.BaseSchema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Document
+@SuperBuilder(toBuilder = true)
+public class Recruitment extends BaseSchema {
+
+    플랫폼 platform;
+    직무 jobCategory;
+    String title;
+    LocalDateTime deadline;
+    String applicationUrl;
+    Company company;
+    Metadata metadata;
+    Integer viewCount;
+
+    @Getter
+    @ToString
+    @AllArgsConstructor(staticName = "of")
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Company {
+
+        String image;
+        String name;
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class CareerRange {
+
+        int start;
+        int end;
+
+        public static CareerRange of(int start, int end) {
+
+            if (start < 0 || end < 0) throw new IllegalArgumentException("start/end must be ≥ 0");
+            if (start > end) throw new IllegalArgumentException("start must be ≤ end");
+
+            return new CareerRange(start, end);
+        }
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor(staticName = "of")
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Metadata {
+
+        채용_유형 recruitmentType;
+        학력_조건 educations;
+        CareerRange careers;
+        지역 region;
+        마감_유형 deadlineType;
+    }
+}
+
