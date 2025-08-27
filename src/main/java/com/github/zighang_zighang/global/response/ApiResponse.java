ef15ea2ee6ad5c6db7e7b4d6287635adb0cb161e
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-28 00:25:20 +0900

    [ZH-15] feat(response): 공통 응답 dto 생성

---
+++ src/main/java/com/github/zighang_zighang/global/response/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.github.zighang_zighang.global.response;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final String code;
+    private final String message;
+    private final T data;
+
+    public static ApiResponse<Void> ok() {
+
+        return ok(null);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+
+        return new ApiResponse<>(true, null, null, data);
+    }
+
+    public static <T> ApiResponse<T> error(String code, String message) {
+
+        return new ApiResponse<>(false, code, message, null);
+    }
+}
