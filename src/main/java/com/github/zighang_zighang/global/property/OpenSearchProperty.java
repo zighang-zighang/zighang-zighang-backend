751b9de0656bbc7233428bbbf8e32f79bbb5c2c3
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-29 03:17:24 +0900

    [ZH-63] chore(property): 스프링 설정 파일에 OpenSearch 관련 설정 추가

---
+++ src/main/java/com/github/zighang_zighang/global/property/OpenSearchProperty.java
@@ -0,0 +1,31 @@
+package com.github.zighang_zighang.global.property;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "spring.opensearch")
+public class OpenSearchProperty {
+
+    @NotBlank
+    String host;
+
+    @Min(1)
+    @Max(65535)
+    @NotNull
+    Integer port;
+
+    @NotBlank
+    String username;
+
+    @NotBlank
+    String password;
+}
