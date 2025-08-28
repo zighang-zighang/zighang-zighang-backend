1f2cccd52a625044d2b0e14245eb86817aea2de8
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-28 19:36:20 +0900

    [0.1.0] (ZH-15) 공통 응답 DTO 생성 (#13)

---
+++ src/main/java/com/github/zighang_zighang/global/response/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.github.zighang_zighang.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final String code;
+    private final String message;
+    private final T data;
+
+    public static ApiResponse<Void> ok() {
+
+        return ok(null);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+
+        return new ApiResponse<>(true, null, null, data);
+    }
+
+    public static <T> ApiResponse<T> error(String code, String message) {
+
+        return new ApiResponse<>(false, code, message, null);
+    }
+}
