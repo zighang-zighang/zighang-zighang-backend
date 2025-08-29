fc53dfb7acaf241e54daac7d25f0fb1393f341e2
Son Daehyeon <sondaehyeon01@gmail.com>
2025-08-29 22:14:32 +0900

    [ZH-63] feat(opensearch): OpenSearch 클라이언트 구현

---
+++ src/main/java/com/github/zighang_zighang/global/config/OpenSearchConfig.java
@@ -0,0 +1,86 @@
+package com.github.zighang_zighang.global.config;
+
+import com.github.zighang_zighang.global.property.OpenSearchProperty;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.hc.client5.http.auth.AuthScope;
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.URIScheme;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.httpclient5.ApacheHttpClient5Transport;
+import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+@RequiredArgsConstructor
+public class OpenSearchConfig {
+
+    private final OpenSearchProperty openSearchProperty;
+
+    @Bean
+    @SneakyThrows
+    public OpenSearchClient openSearchClient() {
+
+        HttpHost host = new HttpHost(
+                URIScheme.HTTPS.getId(),
+                openSearchProperty.getHost(),
+                openSearchProperty.getPort()
+        );
+
+        ApacheHttpClient5Transport transport = ApacheHttpClient5TransportBuilder.builder(host)
+                .setHttpClientConfigCallback(builder -> builder
+                        .setDefaultCredentialsProvider(basicCredentialsProvider())
+                        .setConnectionManager(
+                                PoolingAsyncClientConnectionManagerBuilder.create()
+                                        .setTlsStrategy(
+                                                ClientTlsStrategyBuilder.create()
+                                                        .setSslContext(sslContext())
+                                                        .buildAsync()
+                                        )
+                                        .build()
+                        )
+                )
+                .build();
+
+        return new OpenSearchClient(transport);
+    }
+
+    @Bean
+    public BasicCredentialsProvider basicCredentialsProvider() {
+
+        HttpHost host = new HttpHost(URIScheme.HTTPS.getId(), openSearchProperty.getHost(), openSearchProperty.getPort());
+
+        BasicCredentialsProvider credentials = new BasicCredentialsProvider();
+
+        credentials.setCredentials(
+                new AuthScope(host),
+                new UsernamePasswordCredentials(
+                        openSearchProperty.getUsername(),
+                        openSearchProperty.getPassword().toCharArray()
+                )
+        );
+
+        return credentials;
+    }
+
+    @Bean
+    @SneakyThrows({NoSuchAlgorithmException.class, KeyStoreException.class, KeyManagementException.class})
+    public SSLContext sslContext() {
+
+        return SSLContextBuilder
+                .create()
+                .loadTrustMaterial(null, (chains, authType) -> true)
+                .build();
+    }
+}
