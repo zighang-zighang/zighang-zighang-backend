d9733463ae985486f4c0e209d9bf9e756d9e22fc
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-01 02:18:59 +0900

    [ZH-83] fix(cache): 캐시용 ObjectMapper의 RCE 대응

--- src/main/java/com/github/zighang_zighang/global/config/CacheConfig.java
+++ src/main/java/com/github/zighang_zighang/global/config/CacheConfig.java
@@ -2,6 +2,8 @@ package com.github.zighang_zighang.global.config;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -45,9 +47,12 @@ public class CacheConfig {
         ObjectMapper objectMapper = new ObjectMapper();
 
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         objectMapper.activateDefaultTyping(
-                objectMapper.getPolymorphicTypeValidator(),
+                BasicPolymorphicTypeValidator.builder()
+                        .allowIfBaseType("com.github.zighang_zighang")
+                        .build(),
                 ObjectMapper.DefaultTyping.NON_FINAL,
                 JsonTypeInfo.As.PROPERTY
         );
