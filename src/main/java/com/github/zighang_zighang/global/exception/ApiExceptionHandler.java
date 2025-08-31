0e1887196eed1d329ad8aca188e441545c12c40f
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-01 02:16:39 +0900

    [ZH-83] fix(exception): MethodArgumentNotValidException`을 `ConstraintViolationException`로 변경

--- src/main/java/com/github/zighang_zighang/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/github/zighang_zighang/global/exception/ApiExceptionHandler.java
@@ -4,10 +4,11 @@ import com.github.zighang_zighang.global.response.ApiResponse;
 import io.sentry.Sentry;
 import io.sentry.protocol.Request;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -37,17 +38,17 @@ public class ApiExceptionHandler {
         return GlobalExceptionCode.BODY_NOT_READABLE.toResponse();
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ApiResponse<?> constraintViolationException(ConstraintViolationException e) {
 
-        if (Objects.isNull(e.getBindingResult().getFieldError())) {
+        ConstraintViolation<?> violation = e.getConstraintViolations().iterator().next();
 
-            return ApiResponse.error(GlobalExceptionCode.BODY_VALIDATION_FAILED.getCode(), e.getMessage());
+        String field = violation.getPropertyPath().toString();
+        if (field.contains(".")) {
+            field = field.substring(field.lastIndexOf(".") + 1);
         }
 
-        String field = e.getBindingResult().getFieldError().getField();
-        String message = e.getBindingResult().getFieldError().getDefaultMessage();
-        String errorMessage = String.format("%s은(는) %s", field, message);
+        String errorMessage = String.format("%s은(는) %s.", field, violation.getMessage());
 
         return ApiResponse.error(GlobalExceptionCode.BODY_VALIDATION_FAILED.getCode(), errorMessage);
     }
