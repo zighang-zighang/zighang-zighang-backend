a23c178e74f5c88d5ba680a65a85c42dd2050df9
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-03 20:20:26 +0900

    [ZH-114] refactor(recruitment): 락 제거 ^^

--- src/main/java/com/github/zighang_zighang/domain/recruitment/repository/RecruitmentViewRepository.java
+++ src/main/java/com/github/zighang_zighang/domain/recruitment/repository/RecruitmentViewRepository.java
@@ -2,9 +2,7 @@ package com.github.zighang_zighang.domain.recruitment.repository;
 
 import com.github.zighang_zighang.domain.recruitment.entity.RecruitmentView;
 import com.github.zighang_zighang.domain.user.entity.User;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,7 +10,6 @@ import java.util.UUID;
 
 public interface RecruitmentViewRepository extends JpaRepository<RecruitmentView, UUID> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     boolean existsByUserAndRecruitmentId(User user, UUID recruitmentId);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
