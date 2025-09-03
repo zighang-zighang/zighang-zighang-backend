3e31088a2d66ef808f80993ef3ffb35fe9af1c0a
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-03 20:54:48 +0900

    [ZH-119] chore(recruitment): RecruitmentView에 락 제거

--- src/main/java/com/github/zighang_zighang/domain/recruitment/repository/RecruitmentViewRepository.java
+++ src/main/java/com/github/zighang_zighang/domain/recruitment/repository/RecruitmentViewRepository.java
@@ -2,15 +2,12 @@ package com.github.zighang_zighang.domain.recruitment.repository;
 
 import com.github.zighang_zighang.domain.recruitment.entity.RecruitmentView;
 import com.github.zighang_zighang.domain.user.entity.User;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RecruitmentViewRepository extends JpaRepository<RecruitmentView, UUID> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<RecruitmentView> findByUserAndRecruitmentId(User user, UUID recruitmentId);
 }
