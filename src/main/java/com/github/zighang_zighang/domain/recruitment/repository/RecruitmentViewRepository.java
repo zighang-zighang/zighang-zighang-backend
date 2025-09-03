536e5b1b423ae8566fcfc5230e3cc9f00a478e64
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-03 19:40:33 +0900

    [ZH-119] feat(repository): RecruitmentView 관련 Repository 구현

---
+++ src/main/java/com/github/zighang_zighang/domain/recruitment/repository/RecruitmentViewRepository.java
@@ -0,0 +1,13 @@
+package com.github.zighang_zighang.domain.recruitment.repository;
+
+import com.github.zighang_zighang.domain.recruitment.entity.RecruitmentView;
+import com.github.zighang_zighang.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RecruitmentViewRepository extends JpaRepository<RecruitmentView, UUID> {
+
+    Optional<RecruitmentView> findByUserAndRecruitmentId(User user, UUID recruitmentId);
+}
