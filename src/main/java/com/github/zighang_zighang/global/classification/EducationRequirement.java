e457491e96dc1233ea3afa1843934eb369baa778
EunjinWoo <esther0904@naver.com>
2025-09-07 18:26:00 +0900

    [ZH-82] chore(onboarding): 공고 필터링/온보딩 시 필요한 ENUM 값 추가

---
+++ src/main/java/com/github/zighang_zighang/global/classification/EducationRequirement.java
@@ -0,0 +1,11 @@
+package com.github.zighang_zighang.global.classification;
+
+public enum EducationRequirement {
+    학력_무관,
+    고졸,
+    초대졸,
+    학사,
+    석사,
+    박사
+}
+
---
+++ src/main/java/com/github/zighang_zighang/global/classification/Region.java
@@ -0,0 +1,23 @@
+package com.github.zighang_zighang.global.classification;
+
+public enum Region {
+    서울,
+    경기,
+    인천,
+    부산,
+    대구,
+    광주,
+    대전,
+    울산,
+    세종,
+    강원,
+    경남,
+    경북,
+    전남,
+    전북,
+    충남,
+    충북,
+    제주,
+    해외,
+    기타
+}
