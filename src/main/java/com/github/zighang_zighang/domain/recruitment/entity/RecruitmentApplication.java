c0c3e1eb2450b24458a5528cb27ea19cbca643bf
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-12 13:44:20 +0900

    hotfix(application): unique 제약 제거
    Signed-off-by: Son Daehyeon <sondaehyeon01@gmail.com>

--- src/main/java/com/github/zighang_zighang/domain/recruitment/entity/RecruitmentApplication.java
+++ src/main/java/com/github/zighang_zighang/domain/recruitment/entity/RecruitmentApplication.java
@@ -8,15 +8,10 @@ import lombok.*;
 import java.util.UUID;
 
 @Entity
-@Table(
-        indexes = {
-                @Index(columnList = "user_id"),
-                @Index(columnList = "recruitment_id")
-        },
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"user_id", "recruitment_id"})
-        }
-)
+@Table(indexes = {
+        @Index(columnList = "user_id"),
+        @Index(columnList = "recruitment_id")
+})
 @Getter
 @Setter
 @Builder
