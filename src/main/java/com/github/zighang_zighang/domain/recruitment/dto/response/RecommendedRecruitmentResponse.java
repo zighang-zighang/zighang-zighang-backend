ad484406a689a327b593400034340a0062f3a081
esther <92513850+EunjinWoo@users.noreply.github.com>
2025-09-16 02:01:14 +0900

    [0.8.0] (ZH-77) 개인화 공고 추천 (#34)

---
+++ src/main/java/com/github/zighang_zighang/domain/recruitment/dto/response/RecommendedRecruitmentResponse.java
@@ -0,0 +1,80 @@
+package com.github.zighang_zighang.domain.recruitment.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.github.zighang_zighang.domain.recruitment.constant.Education;
+import com.github.zighang_zighang.domain.recruitment.constant.EmploymentType;
+import com.github.zighang_zighang.domain.recruitment.entity.Recruitment;
+import com.github.zighang_zighang.global.classification.Job;
+import com.github.zighang_zighang.global.classification.JobCategory;
+import com.github.zighang_zighang.global.classification.Location;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true) // vector나 기타 불필요 필드는 무시 가능하게 해 줌
+public class RecommendedRecruitmentResponse {
+
+    private UUID id;
+    private String title;
+    private String description; // RecruitmentResponse에는 없었지만 응답에는 있음
+    private String recruitmentUrl;
+    private String imageUrl;
+
+    private List<Location> locations;
+    private Integer minExperience;
+    private Integer maxExperience;
+    private List<Education> educations;
+
+//     TODO: OffsetDateTime(ex.1750047240.000000000) -> LocalDateTime 형식으로 수정
+//    private OffsetDateTime startDate;
+//    private OffsetDateTime endDate;
+
+//    private String deadlineType;
+    private List<EmploymentType> employmentTypes;
+    private List<Job> jobs;
+    private List<JobCategory> jobCategories;
+
+    private String companyName;
+    private String companyDescription;
+    private String companyImageUrl;
+//    private String companySize;
+
+    // TODO: 피그마 보고 키워드 등 추가 필요 필드 추가
+    @Schema(description = "북마크 여부")
+    private Boolean isBookmarked;
+
+    @Schema(description = "추천 이유 (59자 이내)")
+    private String reason;
+
+    public static RecommendedRecruitmentResponse from(Recruitment recruitment, boolean bookmarked, String reason) {
+        return RecommendedRecruitmentResponse.of(
+                recruitment.getId(),
+                recruitment.getTitle(),
+                recruitment.getDescription(),
+                recruitment.getRecruitmentUrl(),
+                recruitment.getImageUrl(),
+                recruitment.getLocations(),
+                recruitment.getMaxExperience(),
+                recruitment.getMaxExperience(),
+                recruitment.getEducations(),
+                recruitment.getEmploymentTypes(),
+                recruitment.getJobs(),
+                recruitment.getJobCategories(),
+                recruitment.getCompanyName(),
+                recruitment.getCompanyDescription(),
+                recruitment.getCompanyImageUrl(),
+                bookmarked,
+                reason
+        );
+    }
+
+}
+
--- src/main/java/com/github/zighang_zighang/global/infra/opensearch/service/OpenSearchService.java
+++ src/main/java/com/github/zighang_zighang/global/infra/opensearch/service/OpenSearchService.java
@@ -1,23 +1,33 @@
 package com.github.zighang_zighang.global.infra.opensearch.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.zighang_zighang.domain.recruitment.dto.response.RecommendedRecruitmentResponse;
+import com.github.zighang_zighang.domain.resume.__embedding.repository.ResumeEmbeddingRepository;
+import com.github.zighang_zighang.domain.resume.exception.ResumeException;
+import com.github.zighang_zighang.domain.user.entity.User;
 import com.github.zighang_zighang.global.infra.opensearch.exception.OpenSearchException;
 import lombok.RequiredArgsConstructor;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch.core.IndexRequest;
 import org.opensearch.client.opensearch.core.IndexResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class OpenSearchService {
 
     private final OpenSearchClient client;
-    private final ObjectMapper objectMapper;
+    private final WebClient.Builder webClientBuilder;
+    private final ResumeEmbeddingRepository resumeEmbeddingRepository;
 
     public void indexResumeEmbedding(UUID resumeId, Object embedding) {
         try {
@@ -39,4 +49,24 @@ public class OpenSearchService {
             throw OpenSearchException.EXTRACT_FAILED.toException();
         }
     }
+
+    // TODO: 추후 MySQL 임베딩이 아닌 오픈서치에서 바로 가져오는 방향으로 수정해볼 예정
+    // TODO: 최신 1개의 임베딩이 아니라 사용자가 가진 자기소개서 전체의 종합(평균) 임베딩 값으로 수정 필요
+    // TODO: +기록, 북마크, 메모 기반 추천도 가능하게 수정
+    public float[] getLatestResumeEmbedding(User user) {
+        String embeddingStr = resumeEmbeddingRepository.findLatestEmbeddingByUser(user)
+                .orElseThrow(ResumeException.NOT_FOUND::toException);
+
+        return parseEmbedding(embeddingStr);
+    }
+
+    private float[] parseEmbedding(String embeddingStr) {
+        String[] parts = embeddingStr.replace("[", "").replace("]", "").split(",");
+        float[] result = new float[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            result[i] = Float.parseFloat(parts[i].trim());
+        }
+        return result;
+    }
+
 }
