4f39ce7a7b5c0c8118850fcd494b03514e17636d
EunjinWoo <esther0904@naver.com>
2025-09-16 02:30:43 +0900

    hotfix(recommend): (임시) 오픈서치 SSL 설정

--- src/main/java/com/github/zighang_zighang/global/config/WebClientConfig.java
+++ src/main/java/com/github/zighang_zighang/global/config/WebClientConfig.java
@@ -1,13 +1,35 @@
 package com.github.zighang_zighang.global.config;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+
+        try {
+            // SSL 인증서 검증 비활성화 (테스트용)
+            SslContext sslContext = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+
+            HttpClient httpClient = HttpClient.create()
+                    .secure(t -> t.sslContext(sslContext));
+
+            return WebClient.builder()
+                    .clientConnector(new ReactorClientHttpConnector(httpClient));
+
+        } catch (Exception e) {
+            throw new IllegalStateException("WebClient SSL 설정 초기화 실패", e);
+        }
+
+//        return WebClient.builder();
     }
 }
