6b7d36a6895b9431eda248eb014ee07ac4095440
Son Daehyeon <sondaehyeon01@gmail.com>
2025-09-17 01:32:46 +0900

    hotfix(jackson): 시간 데이터가 배열로 직렬화되던 이슈 해결
    Signed-off-by: Son Daehyeon <sondaehyeon01@gmail.com>

--- src/main/java/com/github/zighang_zighang/global/config/JacksonConfig.java
+++ src/main/java/com/github/zighang_zighang/global/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.github.zighang_zighang.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper().registerModule(new JavaTimeModule());
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
